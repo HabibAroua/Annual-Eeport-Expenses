ce25540f8323f999a4d5074aaa43c14a8e9c08fa
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-12-01 23:09:18 +0100

    Create Data.cs

---
+++ Model/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Data
+    {
+        public Report [] Reports
+        {
+            get;
+            set;
+        }
+
+        public Data()
+        {
+            Reports = new Report[12];
+        }
+
+    }
+}
