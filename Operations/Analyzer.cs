74fc0b2390eeaec68435f370fe5e7d0e3a432e4a
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-12-02 01:02:34 +0100

    Create Analyzer.cs

---
+++ Operations/Analyzer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+namespace Operations
+{
+    public class Analyzer
+    {
+
+        public Data MyData
+        {
+            get;
+            set;
+        }
+
+        public Analyzer()
+        {
+            MyData = new Data();
+        }
+
+        public double Sum()
+        {
+            return 0;
+        }
+    }
+}
