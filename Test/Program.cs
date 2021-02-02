2c8df224b0db8a1878974adc9a4cfad8668f0039
Habib Aroua <HabibAroua@users.noreply.github.com>
2021-02-02 23:32:42 +0100

    Create Program.cs

---
+++ Test/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void CreateFolder()
+        {
+            string root = @"C:\habib2021";
+            // If directory does not exist, create it. 
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(root);
+            }
+        }
+
+        static void CreateFile()
+        {
+            string path = @"C:\habib2021\MyTest.html";
+
+            try
+            {
+                string ch = "<html><body><h1>Hello Report</h1></body></html>";
+                // Create the file, or overwrite if the file exists.
+                using (FileStream fs = File.Create(path))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes(ch);
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                }
+
+                // Open the stream and read it back.
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CreateFolder();
+            CreateFile();
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+    }
+}
